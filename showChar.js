0d0c6c896033c11a4d23fbe6a354baf28bc9357b
D-Savel <dad.savel@gmail.com>
2021-02-16 17:04:49 +0100

    update auteur, titre livre, nb char showchar.js

--- showChar.js
+++ showChar.js
@@ -1,4 +1,7 @@
 let texte = "C'était à Mégara, faubourg de Carthage, dans les jardins d'Hamilcar."
+let auteur = 'Gustave Flaubert'
+let titreLivre = 'Salammbô'
+console.log(`La phrase : ${texte} est extraite du livre ${titreLivre} de ${auteur}\nCette dernière contient ${texte.length} caractères\n`)
 for (let index = 0; index < texte.length; index++) {
   console.log(`Le caractère ${texte[index]} est à l'index ${index}`)
 }
