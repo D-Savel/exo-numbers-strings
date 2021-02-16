a84822f347c730ade559d28aa93a018e19e0e7fe
D-Savel <dad.savel@gmail.com>
2021-02-16 16:54:01 +0100

    Résolution exo decimal.js, convert.js, nbchar.js, showchar.js

---
+++ convert.js
@@ -0,0 +1,6 @@
+let numbers = [10, 15, 16, 5005, 52390903]
+for (number of numbers) {
+  console.log(`La conversion du nombre ${number} en binaire est égale à ${number.toString(2)}`)
+  console.log(`La conversion du nombre ${number} en octal est égale à ${number.toString(8)}`)
+  console.log(`La conversion du nombre ${number} en hexadécimal est égale à ${number.toString(16)}\n`)
+}
---
+++ showChar.js
@@ -0,0 +1,4 @@
+let texte = "C'était à Mégara, faubourg de Carthage, dans les jardins d'Hamilcar."
+for (let index = 0; index < texte.length; index++) {
+  console.log(`Le caractère ${texte[index]} est à l'index ${index}`)
+}
